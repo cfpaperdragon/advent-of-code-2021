fe368dc1361e9fe69c0f01ca3219d3438dcecbf2
Claudia Frutuoso <claudia.frutuoso@gmail.com>
2021-12-14 18:19:16 +0000

    day 14 the good way

--- code/day14.py
+++ code/day14.py
@@ -1,5 +1,6 @@
 # day04
 import common
+from datetime import datetime
 
 def parse_inputs(input_list):
     template = input_list[0]
@@ -85,4 +86,70 @@ def calculate_part2():
 
 # execute
 # calculate_part1()
-calculate_part2()
+# calculate_part2()
+
+# I'd like to redo it the right way.
+# meaning, count the pairs and then count the letters
+# it is similar of what I did but for all steps
+# when using pairs, only count the last char of each pair plus the first char
+
+def apply_template_count_pairs(template, rules, number_steps):
+    count = 0
+    pairs = {}
+    # initialize pairs
+    for i in range(1, len(template)):
+        pair = template[i-1:i+1]
+        if pair in pairs.keys():
+            pairs[pair] += 1
+        else:
+            pairs[pair] = 1
+    while count < number_steps:
+        count += 1
+        new_pairs = {}
+        for pair in pairs.keys():
+            number_of_pairs = pairs[pair]
+            new_element = rules[pair]
+            new_pair_1 = pair[0] + new_element
+            new_pair_2 = new_element + pair[1]
+            if new_pair_1 in new_pairs.keys():
+                new_pairs[new_pair_1] += number_of_pairs
+            else:
+                new_pairs[new_pair_1] = number_of_pairs
+
+            if new_pair_2 in new_pairs.keys():
+                new_pairs[new_pair_2] += number_of_pairs
+            else:
+                new_pairs[new_pair_2] = number_of_pairs
+        pairs = new_pairs
+    return pairs
+
+def count_elements_with_pairs(pairs):
+    elements = {}
+    for p in pairs.keys():
+        element = p[1] # only last character
+        if element in elements.keys():
+            elements[element] += pairs[p]
+        else:
+            elements[element] = pairs[p]
+    return elements
+
+
+def calculate_the_good_way(steps=10):
+    inputs = common.read_input_to_function_list("input//day14//input.txt", str)
+    template, rules = parse_inputs(inputs)
+    pairs = apply_template_count_pairs(template, rules, steps)
+    # print(pairs)
+    elements = count_elements_with_pairs(pairs)
+    # add the first character
+    elements[template[0]] += 1
+    # print(elements)
+    print(max(elements.values()) - min(elements.values()))
+
+
+before = datetime.now()
+
+# calculate_the_good_way(10)
+calculate_the_good_way(40)
+after = datetime.now()
+
+print("execution time", after - before)
