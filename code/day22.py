d0eb86524db7d1482e671c8b80efb8668caffa00
Claudia Frutuoso <claudia.frutuoso@gmail.com>
2021-12-23 12:37:48 +0000

    day 22 incomplete

---
+++ code/day22.py
@@ -0,0 +1,15 @@
+# day22
+import common
+
+def calculate_part1():
+    inputs = common.read_input_to_function_list("input//day22//example01.txt", str)
+    print(inputs)
+    #do other stuff
+
+def calculate_part2():
+    common.echo("part 2")
+    #do other stuff
+
+# execute
+calculate_part1()
+calculate_part2()
